7c63eff7e25354727b67467b599d8aae72719e86
penach0 <josemanuel.penacho@gmail.com>
2022-10-10 12:35:12 +0100

    Add test for Board#get_square
    - Test checks if method returns nil when a square is played

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,15 +5,20 @@ describe Board do
   subject(:board) { described_class.new }
 
   describe '#get_square' do
-    let(:square) { instance_double('Square', value: 5) }
+    let(:valid_square) { instance_double('Square', value: 5) }
+    let(:played_square) { instance_double('Square', value: 'X') }
 
     context 'when number is valid' do
       it 'returns the corresponding square' do
         query_square = board.get_square(5)
-        expect(query_square.value).to eq(square.value)
+        expect(query_square.value).to eq(valid_square.value)
       end
       it 'returns nil if the number is not in range (1..9)' do
         expect(board.get_square(11)).to be nil
+      end
+      it 'returns nil if the square is already played' do
+        board.board[1][1] = played_square
+        expect(board.get_square(5)).to be nil
       end
     end
   end
