d44740fe0f4f91f0ef7da19b9cde240f867b8658
penach0 <josemanuel.penacho@gmail.com>
2022-10-10 18:24:38 +0100

    Add test to Board#get_positions
    - Test that the method returns a empty array when the board is empty
    - Having trouble testing the other functionalities of the method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,13 +13,38 @@ describe Board do
         query_square = board.get_square(5)
         expect(query_square.value).to eq(valid_square.value)
       end
+    end
+    context 'when number is invalid' do
       it 'returns nil if the number is not in range (1..9)' do
         expect(board.get_square(11)).to be nil
       end
+
       it 'returns nil if the square is already played' do
         board.board[1][1] = played_square
         expect(board.get_square(5)).to be nil
       end
+    end
+  end
+
+  describe '#get_positions' do
+    context 'when no plays have been made' do
+      it 'returns an empty array' do
+        expect(board.get_positions('X')).to eq([])
+      end
+    end
+    context 'when plays have been made' do
+      before do
+        @full_board = board.board.map do |square|
+          case square.position
+          when 1, 3, 5, 8 then square.value = 'X'
+          when 2, 4, 6, 7, 9 then square.value = 'O'
+          end
+        end
+      end
+      it 'returns array when play is X' do
+        expect(@full_board.get_positions('X')).to eq([1, 3, 5, 8])
+      end
+      it 'returns array when play is O'
     end
   end
 end
