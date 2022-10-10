343af9d9af7266027454aba525a5f94f51c7c6bb
penach0 <josemanuel.penacho@gmail.com>
2022-10-10 20:53:47 +0100

    Move #pick_play to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,16 @@ class Game
                        [1, 5, 9], [3, 5, 7]]
   end
 
+  def pick_play
+    print 'Pick your choice (Crosses or Circles): '
+    loop do
+      play = gets.chomp.downcase
+      return play if %w[crosses circles].include?(play)
+
+      print 'That is not a valid option. Try again:'
+    end
+  end
+
   def game_won?(board, play)
     @win_conditions.each do |line|
       return true if (line - board.get_positions(play)).empty?
