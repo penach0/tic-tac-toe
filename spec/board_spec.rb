ac93aca220144bc008b723d1fe4792a493d064e4
penach0 <josemanuel.penacho@gmail.com>
2022-10-12 13:58:03 +0100

    Create tests for Board#full?
    - Tests cover empty board, board half full and board full

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,17 @@ require_relative '../lib/square'
 describe Board do
   subject(:board) { described_class.new }
 
+  def fill_board
+    board.board.each do |row|
+      row.each do |square|
+        case square.position
+        when 1, 3, 5, 8 then square.value = 'X'
+        when 2, 4, 6, 7, 9 then square.value = 'O'
+        end
+      end
+    end
+  end
+
   describe '#get_square' do
     let(:valid_square) { instance_double('Square', value: 5) }
     let(:played_square) { instance_double('Square', value: 'X') }
@@ -34,20 +45,36 @@ describe Board do
     end
     context 'when plays have been made' do
       before do
-        board.board.each do |row|
-          row.each do |square|
-            case square.position
-            when 1, 3, 5, 8 then square.value = 'X'
-            when 2, 4, 6, 7, 9 then square.value = 'O'
-            end
-          end
-        end
+        fill_board
       end
       it 'returns array when play is X' do
         expect(board.get_positions('X')).to eq([1, 3, 5, 8])
       end
       it 'returns array when play is O' do
         expect(board.get_positions('O')).to eq([2, 4, 6, 7, 9])
+      end
+    end
+  end
+
+  describe '#full?' do
+    context 'when board is empty' do
+      it 'returns false' do
+        expect(board).not_to be_full
+      end
+    end
+
+    context 'when board is not full' do
+      it 'returns false' do
+        board.board[0][0].value = 'X'
+        board.board[2][1].value = 'O'
+        expect(board).not_to be_full
+      end
+    end
+
+    context 'when the board is full' do
+      it 'returns true' do
+        fill_board
+        expect(board).to be_full
       end
     end
   end
